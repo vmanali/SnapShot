e24c2c8d88c78d0556c8ed82500d1a84c4304911
Manali Velma <manali.velma@brighthr.com>
2022-08-22 01:43:16 +0100

    base url, specPattern and project id added

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: 'i7eeg5',
+  projectId: 'uow1oq',
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/snapshot-regression-pack/*.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:3000/SnapShot#/SnapScout'
   },
 })
